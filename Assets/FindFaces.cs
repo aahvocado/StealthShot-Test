d14595faaee5b2dbc09d22ff718d7f7d90a87c4a
john <axis.of.idle@gmail.com>
2013-07-29 14:03:16 +0100

    Assigning length of vertice array

--- Assets/FindFaces.cs
+++ Assets/FindFaces.cs
@@ -4,25 +4,27 @@ using System.Collections.Generic;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 
 public class FindFaces : MonoBehaviour {
-GameObject meshHolder;
+public GameObject meshHolder;
 GameObject player;
 bool lastHit;
 bool meshDrawn;
 int numberOfRays;
 int vectorPointNumber = 0;
 float angle;
-public List<Vector3> vectorList;
-public List<Vector3> vectorList2;
+//public List<Vector3> vectorList;
+//public List<Vector3> vectorList2;
 Vector3 newVertices;	
 Vector3 prevVector;
 Vector3 _playerPos;
 Vector3 tmp;
 Vector3 tmp2;
 RaycastHit hit;
-Mesh mesh;
+//Mesh mesh;
 public Vector3[] vertices;
-public Vector3 movePos;
-public Vector3 rayPos;
+public Vector3[] normals;
+public int[] triangles;
+Vector3 movePos;
+Vector3 rayPos;
 	
 	void Start () 
 	{			
@@ -41,24 +43,55 @@ public Vector3 rayPos;
 		{	
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);							
 			movePos = new Vector3(mousePos.x, mousePos.y, transform.position.z);
-			rayPos  = new Vector3(mousePos.x, mousePos.y, 0);
-				
+			// 2nd raycast variable is a direction, so mousepos needs to be changed to a direction for raycast
+			rayPos = movePos - transform.position;		
 				if (Physics.Raycast(transform.position, rayPos, out hit))
 			      	{		
-						// assign mesh of object hit by ray 
+						// highlight object being hit
+						hit.collider.gameObject.renderer.material.color = Color.red;						
+						// get mesh of object hit by ray 
 						Mesh meshHit = hit.collider.gameObject.GetComponent<MeshFilter>().mesh;	
-						// determine size of array for vertices of mesh
+						// determine size of vertices array for vertices of mesh
 						vertices = new Vector3[meshHit.vertices.Length];
-						// translate vertices of mesh into world space
+						// determine size of vertices array using only vertices that are facing down (i.e. the bottom of the polygon)
+						
+						// translate each vertice from local into world space
+						vertices = meshHit.vertices;
+						
+						
 						for (int i=0; i<meshHit.vertices.Length; i++)
-						{
-						vertices[i] = transform.TransformPoint(meshHit.vertices[i]);
-						}
-						Debug.Log(vertices[0]);
-						Debug.Log(vertices[1]);
-						Debug.DrawLine(transform.position, movePos);						
+							{
+							// adjust for scale
+							vertices[i] = new Vector3 (vertices[i].x*hit.collider.gameObject.transform.lossyScale.x, vertices[i].y*hit.collider.gameObject.transform.lossyScale.y,vertices[i].z*hit.collider.gameObject.transform.lossyScale.z);
+							// adjust for world position
+							vertices[i] = hit.collider.gameObject.transform.position - vertices[i];
+							}
+//						Debug.Log(vertices[0].x);
+			
+//						meshHit.GetTriangles
+						// assign normals to array
+						normals = meshHit.normals;
+						triangles = meshHit.triangles;
+						DrawMesh();			
+			
+//						Debug.Log(vertices[0]);
+//						Debug.Log(vertices[1]);
+//						Debug.Log ("hitpoint"+hit.point+"");
+//						Debug.Log ("rayPos"+rayPos+"");
+//						Debug.Log ("movePos"+movePos+"");
 						Debug.DrawLine(transform.position, hit.point);
-					}	
+					}
+				
 
+		}
+	
+	void DrawMesh()
+		
+	{
+	Mesh mesh = meshHolder.GetComponent<MeshFilter>().mesh;
+	mesh.vertices = new Vector3[] {transform.position, vertices[0], vertices[1]};
+	mesh.uv = new Vector2[] {transform.position, vertices[0], vertices[1]};
+	mesh.triangles = new int[] {0, 1, 2};	
+		
 	}
 }
