8fd118b4f5c0cf4c72eea2983a485e27b9693ab0
john <axis.of.idle@gmail.com>
2013-08-16 17:09:08 +0100

    Post edit, functions still work
    Trying to make it so that each polygon is organised into clockwise
    points each sweep and then added to the vector list, rather than all
    the points at once. Should make it easier for start and end point
    allocation for mesh.

--- Assets/FindFaces.cs
+++ Assets/FindFaces.cs
@@ -31,6 +31,7 @@ Vector3[] verticesTemp;
 public Vector3[] normals;
 public int[] triangles;
 public List <Vector3> verticesList;
+public List <Vector3> _verticesList;
 Vector3 verticesDirection;
 public List <Vector3> verticesListTemp;
 float verticesAngle;
@@ -95,50 +96,37 @@ Vector3 rayPos;
 							// adjust for world position
 							vertices[i] = polygon[polyNumber].transform.position - vertices[i];
 							vertices[i].z = transform.position.z;							
-//							// check to see if it's on the opposite side of the poly, assign the vertices to player if it is
+//							// check to see if it's on the opposite side of the poly to the player, leave the vertices if it is
 							Vector3 rayPos = transform.position;
 							RaycastHit hit;	
 							if (!Physics.Linecast (rayPos, vertices[i], out hit)) 
 											{
-//											vertices[i] = transform.position;
-//											Instantiate (marker, hit.point, Quaternion.identity);
-//											vertices[i] = vertices[i-1];
 											verticesList.Add (vertices[i]);
+//											_verticesList.Add (vertices[i]);
 											Debug.DrawLine (vertices[i],rayPos,Color.green);
+											
 											}	
-//							Vector3 rayPos = transform.position-vertices[i];
-//							rayPos.z = transform.position.z;
-//							if (Physics.Raycast (rayPos, vertices[i], out hit))
-//											{
-//											vertices[i] = transform.position;
-//											}							
-//							vertices[i] = new Vector3 ((transform.position.x + vertices[i].x) * 5, (transform.position.y + vertices[i].y) * 5, vertices[i].z);
-//							if (vertices[i] != transform.position)
-//								{
-//									verticesList.Add (vertices[i]);	
-//								}
 							
 							}
-//						polygon[polyNumber].renderer.material.color = Color.white;
 						triangles = meshHit.triangles;
 						polyNumber++;
-						// add current poly vertex number to total number of vertices						
-//							verticesArraySize = verticesArraySize + _verticesArraySize;
+						// add current poly vertex number to total number of vertices										
 							verticesArraySize = verticesArraySize + verticesList.Count;
+							
 						}
 //						_marker = GameObject.FindGameObjectsWithTag("Walls");						
-						AssignVerticesAngles();		
-//						DrawMesh();	
+						AssignVerticesAngles(verticesList);						
+						DrawMesh();	
 		}
 	
 	//establish vertices angle in relation to player, sort in clockwise order
-	public void AssignVerticesAngles()
+	public void AssignVerticesAngles(List<Vector3> verticesAngles)
 	{
-	actualAngle = new float[verticesList.Count];	
-	for (int i=0; i<verticesList.Count; i++)
+	actualAngle = new float[verticesAngles.Count];	
+	for (int i=0; i<verticesAngles.Count; i++)
 		{
 		// find angle of vertices
-		verticesDirection = verticesList[i] - transform.position;
+		verticesDirection = verticesAngles[i] - transform.position;
 		verticesAngle = Vector3.Angle(verticesDirection,Vector3.left);		
 		float dirNum = AngleDir(Vector3.left, verticesDirection, Vector3.forward); 		
 			if(dirNum>0F)
@@ -149,28 +137,8 @@ Vector3 rayPos;
 				{
 				actualAngle[i] += verticesAngle;
 				}			
-		}
-	ArrangeVerticesCW();
-	}
-	
-		//variable to turn acute 180 degree angles into 360 degree angles
-		private float AngleDir(Vector3 fwd, Vector3 targetDir, Vector3 up) {
-			Vector3 perp = Vector3.Cross(fwd, targetDir);
-			float dir = Vector3.Dot(perp, up);		
-			if (dir > 0f) {
-				return 1f;
-			} else if (dir < 0f) {
-				return -1f;
-			} else {
-				return 0f;
-			}
-			
-		}	
-	
-	//rearrange vertices into order moving clockwise around player
-	public void ArrangeVerticesCW()
-	{
-	
+		}		
+	//rearrange vertices into order moving clockwise around player	
 	float sortCheckSize = actualAngle.Length;
 //	Debug.Log (actualAngle.Length);
 	int iV = 0;	
@@ -219,8 +187,23 @@ Vector3 rayPos;
 				}
 			}			
         }	
-	DrawMesh();
+//	DrawMesh();
 	}
+		
+	//variable to turn acute 180 degree angles into 360 degree angles
+		private float AngleDir(Vector3 fwd, Vector3 targetDir, Vector3 up) {
+			Vector3 perp = Vector3.Cross(fwd, targetDir);
+			float dir = Vector3.Dot(perp, up);		
+			if (dir > 0f) {
+				return 1f;
+			} else if (dir < 0f) {
+				return -1f;
+			} else {
+				return 0f;
+			}
+			
+		}		
+		
 	
 	
 	
